a273c2e2b6b47d3c52f94684251f0ea1448b7142
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-07-16 18:53:11 +0300

    integration tests modified

--- tests/IntegrationTests/DoctorIntegrationTests.cs
+++ tests/IntegrationTests/DoctorIntegrationTests.cs
@@ -13,20 +13,44 @@ using Xunit;
 
 namespace tests.IntegrationTests;
 
-public class DoctorIntegrationTests : IClassFixture<ApiWebApplicationFactory>
+public class DoctorIntegrationTests : IClassFixture<ApiWebApplicationFactory>, IDisposable
 {
     private readonly HttpClient _client;
+    private bool _disposed = false;
 
     public DoctorIntegrationTests(ApiWebApplicationFactory factory)
     {
         _client = factory.CreateClient();
     }
 
+    public void Dispose()
+    {
+        CleanUpDatabase().GetAwaiter().GetResult();
+        _disposed = true;
+    }
+
+    private async Task CleanUpDatabase()
+    {
+        var request = "/api/v1/Doctor/all";
+        var response = await _client.GetAsync(request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ListDoctorDto>(responseString);
+            foreach (DoctorDto doc in result.doctorList)
+            {
+                request = "/api/v1/Doctor/delete/" + doc.Id;
+                await _client.DeleteAsync(request);
+            }
+        }
+    }
+
     [Fact]
-    public async Task Post_Create_ValidRequest_ReturnsCreatedStatusCode_ValidProductContentResponse()
+    public async Task Post_Create_ValidRequest_ReturnsCreatedStatusCode_ValidDoctorContentResponse()
     {
         var request = "/api/v1/Doctor/create";
-        var doctor = new CreateDoctorRequest { Name = "new doctor", Type = "new type", Patients = 20 };
+        var doctor = new CreateDoctorRequest { Name = "Dr. Smith", Type = "General", Patients = 50 };
         var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
 
         var response = await _client.PostAsync(request, content);
@@ -40,227 +64,123 @@ public class DoctorIntegrationTests : IClassFixture<ApiWebApplicationFactory>
         Assert.Equal(doctor.Name, result.Name);
         Assert.Equal(doctor.Type, result.Type);
         Assert.Equal(doctor.Patients, result.Patients);
-        
-        EraseAll_CleanUpDB();
-
     }
-    
+
     [Fact]
     public async Task Post_Create_DoctorAlreadyExists_ReturnsBadRequestStatusCode()
     {
-        var request = "/api/v1/Doctor/create";
-        var doctor = new CreateDoctorRequest { Name = "new doctor", Type = "new type", Patients = 20 };
-        var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
 
+        var request = "/api/v1/Doctor/create";
+        var doctor = new CreateDoctorRequest { Name = "Dr. Smith", Type = "General", Patients = 50 };
+        var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
+        
         await _client.PostAsync(request, content);
         var response = await _client.PostAsync(request, content);
-
-        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         
-        EraseAll_CleanUpDB();
+        Assert.Equal(HttpStatusCode.BadRequest,response.StatusCode);
+
+
     }
 
     [Fact]
-    public async Task Put_Update_ValidRequest_ReturnsAcceptedStatusCode_ValidProductContentResponse()
-    {
-        var request = "/api/v1/Doctor/create";
-        var doctor = new CreateDoctorRequest { Name = "new doctor", Type = "new type", Patients = 20 };
-        var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
-
-        var response = await _client.PostAsync(request, content);
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<Doctor>(responseString)!;
-
-        request = "/api/v1/Doctor/update/"+result.Id;
-        var updateDoctor = new UpdateDoctorRequest { Patients = 99 };
-        content = new StringContent(JsonConvert.SerializeObject(updateDoctor), Encoding.UTF8, "application/json");
-
-        response = await _client.PutAsync(request, content);
-        
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-        responseString = await response.Content.ReadAsStringAsync();
-        result = JsonConvert.DeserializeObject<Doctor>(responseString)!;
-
-        Assert.Equal(updateDoctor.Patients, result.Patients);
-        
-        EraseAll_CleanUpDB();
-    }
-
-    [Fact]
-    public async Task Put_Update_DoctorDoesNotExists_ReturnsNotFoundStatusCode()
-    {
-        
-        var request = "/api/v1/Doctor/update/1";
-        var updateDoctor = new UpdateDoctorRequest { Patients = 77 };
-        var content = new StringContent(JsonConvert.SerializeObject(updateDoctor), Encoding.UTF8, "application/json");
-
-        var response = await _client.PutAsync(request, content);
-        
-        Assert.Equal(HttpStatusCode.NotFound,response.StatusCode);
-        
-        EraseAll_CleanUpDB();
-    }
-
-    [Fact]
-    public async Task Delete_Delete_DoctorExists_ReturnsDeletedDoctor()
+    public async Task Put_Update_ValidRequest_ReturnsAcceptedStatusCode()
     {
 
         var request = "/api/v1/Doctor/create";
-        var doctor = new CreateDoctorRequest { Name = "new doctor", Type = "new type", Patients = 20 };
-        var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
-
-        var response = await _client.PostAsync(request, content);
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<Doctor>(responseString)!;
-
-        request = "/api/v1/Doctor/delete/" + result.Id;
-        response = await _client.DeleteAsync(request);
-        
-        Assert.Equal(HttpStatusCode.Accepted,response.StatusCode);
-        
-        EraseAll_CleanUpDB();
-    }
-
-    [Fact]
-    public async Task Delete_Delete_DoctorDoesNotExists_ReturnsNotFoundStatusCode()
-    {
-
-        var request = "/api/v1/Doctor/delete/66";
-
-        var response = await _client.DeleteAsync(request);
-
-        Assert.Equal(HttpStatusCode.NotFound,response.StatusCode);
-        
-        EraseAll_CleanUpDB();
-        
-    }
-
-    [Fact]
-    public async Task Get_GetByName_ValidRequest_ReturnsOKStatusCode()
-    {
-
-        var request = "/api/v1/Doctor/create";
-        var doctor = new CreateDoctorRequest { Name = "new doctor", Type = "new type", Patients = 20 };
-        var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
-
-        var response = await _client.PostAsync(request, content);
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<Doctor>(responseString)!;
-
-        request = "/api/v1/Doctor/name/" + result.Name;
-        response = await _client.GetAsync(request);
-
-        Assert.Equal(HttpStatusCode.OK,response.StatusCode);
-
-        EraseAll_CleanUpDB();
-    }
-
-    [Fact]
-    public async Task Get_GetByName_DoctorDoesNotExists_ReturnsNotFoundStatusCode()
-    {
-
-        var request = "/api/v1/Doctor/name/test";
-
-        var response = await _client.GetAsync(request);
-        
-        Assert.Equal(HttpStatusCode.NotFound,response.StatusCode);
-
-    }
-
-    [Fact]
-    public async Task Get_GetByType_ValidRequest_ReturnsOKStatusCode()
-    {
-
-        var request = "/api/v1/Doctor/create";
-        var doctor = new CreateDoctorRequest { Name = "new doctor", Type = "new type", Patients = 20 };
+        var doctor = new CreateDoctorRequest { Name = "Dr. Smith", Type = "General", Patients = 50 };
         var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
 
         var response = await _client.PostAsync(request, content);
         var responseString = await response.Content.ReadAsStringAsync();
         var result = JsonConvert.DeserializeObject<Doctor>(responseString);
 
-        request = "/api/v1/Doctor/type/" + result.Type;
+        request = "/api/v1/Doctor/update/" + result.Id;
+        var updateDoctor = new UpdateDoctorRequest { Patients = 23 };
+        content = new StringContent(JsonConvert.SerializeObject(updateDoctor), Encoding.UTF8, "application/json");
 
-        response = await _client.GetAsync(request);
+        response = await _client.PutAsync(request, content);
         
         Assert.Equal(HttpStatusCode.OK,response.StatusCode);
 
-        EraseAll_CleanUpDB();
+        responseString = await response.Content.ReadAsStringAsync();
+        result = JsonConvert.DeserializeObject<Doctor>(responseString);
+
+        Assert.Equal(updateDoctor.Patients,result.Patients);
+        
     }
 
     [Fact]
-    public async Task Get_GetByType_NoDoctorExists_ReturnsNotFoundStatusCode()
+    public async Task Put_Upadte_DoctorDoesNotExists_ReturnsNotFoundStatusCode()
     {
-        
-        var request = "/api/v1/Doctor/type/test";
 
-        var response = await _client.GetAsync(request);
+        var request = "/api/v1/Doctor/update/15";
+        var updateDoctor = new UpdateDoctorRequest { Patients = 44 };
+        var content = new StringContent(JsonConvert.SerializeObject(updateDoctor), Encoding.UTF8, "application/json");
+
+        var response = await _client.PutAsync(request, content);
         
         Assert.Equal(HttpStatusCode.NotFound,response.StatusCode);
+
+    }
+    
+    [Fact]
+    public async Task Delete_DoctorExists_ReturnsAcceptedStatusCode()
+    {
+        var request = "/api/v1/Doctor/create";
+        var doctor = new CreateDoctorRequest { Name = "Dr. Johnson", Type = "Pediatrician", Patients = 30 };
+        var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
+
+        var postResponse = await _client.PostAsync(request, content);
+        var postResponseString = await postResponse.Content.ReadAsStringAsync();
+        var createdDoctor = JsonConvert.DeserializeObject<Doctor>(postResponseString);
+
+        request = "/api/v1/Doctor/delete/" + createdDoctor.Id;
+        var deleteResponse = await _client.DeleteAsync(request);
         
-        EraseAll_CleanUpDB();
+        Assert.Equal(HttpStatusCode.Accepted, deleteResponse.StatusCode);
+    }
+
+    [Fact]
+    public async Task Delete_DoctorDoesNotExists_ReturnsNotFoundStatusCode()
+    {
+
+        var request = "/api/v1/Doctor/delete/44";
+
+        var response = await _client.DeleteAsync(request);
+
+        Assert.Equal(HttpStatusCode.NotFound,response.StatusCode);
+        
     }
 
     [Fact]
     public async Task Get_GetById_ValidRequest_ReturnsOKStatusCode()
     {
+
         var request = "/api/v1/Doctor/create";
-        var doctor = new CreateDoctorRequest { Name = "new doctor", Type = "new type", Patients = 2088 };
+        var doctor = new CreateDoctorRequest { Name = "Dr. Smith", Type = "General", Patients = 50 };
         var content = new StringContent(JsonConvert.SerializeObject(doctor), Encoding.UTF8, "application/json");
-        await _client.PostAsync(request, content);
-        
-        var doctor2 = new CreateDoctorRequest { Name = "new doctor2", Type = "new type2", Patients = 33 };
-        content = new StringContent(JsonConvert.SerializeObject(doctor2), Encoding.UTF8, "application/json");
+
         var response = await _client.PostAsync(request, content);
-       
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<Doctor>(responseString)!;
-       
+        var stringResponse = await response.Content.ReadAsStringAsync();
+        var result = JsonConvert.DeserializeObject<Doctor>(stringResponse);
+
         request = "/api/v1/Doctor/id/" + result.Id;
         response = await _client.GetAsync(request);
-        
-        Assert.Equal(HttpStatusCode.OK,response.StatusCode);
-        
-        EraseAll_CleanUpDB();
-        
-    }
-    
-    [Fact]
-    public async Task Get_GetById_DoctorDoesExists_ReturnsNotFoundStatusCode()
-    {
-        
-        var request = "/api/v1/Doctor/id/1";
 
+        Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Get_GetById_DoctorDoesNotExists_ReturnsNotFoundStatusCode()
+    {
+
+        var request = "/api/v1/Doctor/id/33";
+        
         var response = await _client.GetAsync(request);
         
         Assert.Equal(HttpStatusCode.NotFound,response.StatusCode);
-        
-        EraseAll_CleanUpDB();
-    }
-    
-    private async void EraseAll_CleanUpDB()
-    {
-        var request = "/api/v1/Doctor/all";
-        var response = await _client.GetAsync(request);
-
-        var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<ListDoctorDto>(responseString);
-        
-        foreach (DoctorDto doc in result.doctorList)
-        {
-            request = "/api/v1/Doctor/delete/" + doc.Id;
-        
-            await _client.DeleteAsync(request);
-        
-        }
-        
-        Console.WriteLine(result.doctorList);
 
     }
-    
-    
-    
     
     
 }
